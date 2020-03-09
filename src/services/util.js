7d7242386a6febe73368bfb89baabf78910001eb
Mark Wolff <mark.wolff@microsoft.com>
2020-03-09 15:13:39 -0700

    use prod API

--- src/services/util.js
+++ src/services/util.js
@@ -9,4 +9,5 @@ export const formatPrice = (x, currency) => {
 
 // export const productsAPI =
   // 'https://react-shopping-cart-67954.firebaseio.com/products.json';
-export const productsAPI = "http://localhost:8002/api/products";
+// export const productsAPI = "http://localhost:8002/api/products";
+export const productsAPI = "https://reactshoppingcart.azurewebsites.net/api/products";
