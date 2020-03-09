56701bf07396a73afebf1170e2159d4e0ada8ba5
Mark Wolff <mark.wolff@microsoft.com>
2020-03-09 12:46:03 -0700

    add appinsights

--- server/app.js
+++ server/app.js
@@ -6,10 +6,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const port = 8001;
+const port = 8002;
 
 app.get('/api/products', (req, res) => {
   res.sendFile(path.join(__dirname, 'data', 'products.json'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
